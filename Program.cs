5e9d9a7746349bd421bb42f5fc5ffd75ea22b88e
Anton Faren <Anton.faren@elev.ga.ntig.se>
2024-09-17 12:57:24 +0200

    lucktext

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("skriv in din fotbolls idol:");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("vad bra att du valde " + name +" ");
+Console.WriteLine("vem är den bästa fotbollspelaren?");
+String spelare = Console.ReadLine();
+Console.ReadLine();
+Console.WriteLine("är du säker att "+ spelare + " är den bästa?");
+
+string val = Console.ReadLine();
+
+if(val == "nej") {
+    System.Console.WriteLine("exact för ronaldo är bäst");
+}else {
+    Console.WriteLine("fel Ronaldo är bäst");
+}
+
+Console.ReadLine();
